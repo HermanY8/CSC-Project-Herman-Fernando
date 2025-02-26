c14e9a6b7481fa09c0458143ce8023b00c129032
Fernando Jose Diaz <fernando.jose.diaz.o.o@gmail.com>
2025-02-25 16:32:23 -0800

    added comments to the main.py

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ import time
 import random
 
 WIDTH, HEIGHT = 1000, 800
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Clicks")
+WIN = pygame.display.set_mode((WIDTH, HEIGHT)) #Window Screen that we created
+pygame.display.set_caption("Clicks") # The main at the top of the window of our game
 
 
-
+# making a while loop to make sure that our window stays open
 def main():
     run = True
 
     while run:
-        for event in pygame.event.get():
+        for event in pygame.event.get(): # Looking at when user presses x-button to close window
             if event.type == pygame.QUIT:
-                run = False
+                run = False # end for loop
                 break
 
 
 
-    pygame.quit()
+    pygame.quit() #closes window
 
-if __name__ == "__main__":
+if __name__ == "__main__": # makes sure we are running file and not importing it from a different file
     main()
