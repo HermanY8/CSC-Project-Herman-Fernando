31f7a30008ae89c8b17d502213e0dbd3ff2db2a8
HermanY8! <hyanez01@calpoly.edu>
2025-02-25 16:41:09 -0800

    Starting code for game

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 import pygame
 import time
 import random
@@ -8,7 +6,11 @@ WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Clicks")
 
+BG = pygame.image.load("download (1).jpg")
 
+def draw():
+    WIN.blit(BG, (0, 0))
+    pygame.display.update()
 
 def main():
     run = True
@@ -18,6 +20,7 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
+        draw()
 
 
 
