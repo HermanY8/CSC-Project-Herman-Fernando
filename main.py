183c31fa23549ba03df55081371393f88ef9256e
HermanY8! <hyanez01@calpoly.edu>
2025-03-01 13:08:19 -0800

    worked on car class

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 import class_file
 
 WIDTH, HEIGHT = 1000, 800
-WIN = pygame.display.set_mode((WIDTH, HEIGHT)) #Window Screen that we created
+WIN = pygame.display.set_mode((WIDTH, HEIGHT)) # Window Screen that we created
 pygame.display.set_caption("Clicks") # The main at the top of the window of our game
 
 # Scaling background image to fit on the screen
