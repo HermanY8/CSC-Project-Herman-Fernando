d0ebb306a532ba87acbdece9aac2c9df48d2e5d0
Fernando Jose Diaz <fernando.jose.diaz.o.o@gmail.com>
2025-03-07 17:50:47 -0800

    commiting but do not merge

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ import time
 import random
 from tkinter import *
 from threading import Timer
+
+
 import class_file
+
+root = Tk()
+root.title("Clicks")
+root.geometry("1000x800")
+canvas = Canvas( root , width= 1000 , height= 800)
+canvas.pack()
+
+
 
 WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT)) # Window Screen that we created
@@ -12,6 +22,39 @@ pygame.display.set_caption("Clicks") # The main at the top of the window of our 
 pygame.init() # Initializes Pygame modules which will be used in our program
 
 BG = pygame.transform.scale(pygame.image.load("Race_Track.jpg"), (WIDTH, HEIGHT))  # Scaling background image to fit on the screen
+
+
+
+set_time = 10
+num = 0
+timer = None
+
+your_cps = Entry(root)
+your_cps.pack()
+
+
+def cps2():
+    global num, choose, your_cps
+    cps = num / choose
+    your_cps.delete(0,END)
+    your_cps.insert(END, f"Your CPS: {cps:.2f}")
+    print("Your CPS:" + cps)
+
+def addcps():
+    global num
+    num += 1
+
+
+def runcps():
+    global timer, num
+    num = 0
+    timer = Timer(choose, cps2)
+    timer.start
+Click_to_start = Button(root, text = "Click to Star!", padx = 200, pady= 100, command=runcps)
+Click_to_start.pack()
+
+
+
 
 def draw(car, score):
     WIN.blit(BG, (0, 0))  # Draws the background image at coordinates (0, 0) (top left) on the game window
